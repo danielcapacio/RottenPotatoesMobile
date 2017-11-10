9168aea548c7c4560f540f9fd073fc8a10d35756
Daniel Capacio <danielcapacio@gmail.com>
2017-11-09 19:27:04 -0800

    Initial Commit

---
+++ RottenPotatoesMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RottenPotatoesMobile
+//
+//  Created by Daniel Capacio on 2017-11-09.
+//  Copyright © 2017 Daniel Capacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
