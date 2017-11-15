d0e1260f427ddfea9576ed71c690d64105fbd55e
Daniel Capacio <danielcapacio@gmail.com>
2017-11-15 02:21:55 -0800

    Created initial api manager

---
+++ RottenPotatoesMobile/APIManager.swift
@@ -0,0 +1,34 @@
+//
+//  APIManager.swift
+//  RottenPotatoesMobile
+//
+//  Created by Daniel Capacio on 2017-11-15.
+//  Copyright © 2017 Daniel Capacio. All rights reserved.
+//
+
+import Foundation
+
+struct APIManager {
+    
+    static let API_KEY = "" // not committing my api key
+    
+    static let baseUrl = "https://api.themoviedb.org/3"
+    static let baseUrlPoster = "https://image.tmdb.org/t/p/"
+    
+    static let discover = "/discover/movie"
+    static let genres = "/genre/movie/list"
+    static let popularMovies = "/movie/popular"
+    static let nowPlaying = "/movie/now_playing"
+    static let topRated = "/movie/top_rated"
+    static let popularPeople = "/person/popular"
+    
+    enum posterSize: String {
+        case xsmall = "w92"
+        case small = "w154"
+        case medium = "w185"
+        case large = "w342"
+        case big = "w500"
+        case xbig = "w780"
+    }
+    
+}
